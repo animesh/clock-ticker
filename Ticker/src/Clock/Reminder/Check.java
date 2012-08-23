4250847bd742767bc27bc938344636393a120a49
Ani <sharma.animesh@gmail.com>
2012-08-23 13:09:18 +0530

    manipulating clock tutorial

--- Ticker/src/Clock/Reminder/Check.java
+++ Ticker/src/Clock/Reminder/Check.java
@@ -2,14 +2,23 @@ package Clock.Reminder;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.AnalogClock;
+import android.widget.DigitalClock;
 
 public class Check extends Activity
 {
     /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+ 
+		AnalogClock ac = (AnalogClock) findViewById(R.id.analogClock1);
+		//what can i do with AnalogClock?
+ 
+		DigitalClock dc = (DigitalClock) findViewById(R.id.digitalClock1);
+		//what can i do with DigitalClock also? for display only
+ 
+	}
+ 
 }
