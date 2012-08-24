218935bb50f2b55bce1a4a48e87ff70b3a4a7a75
Ani <sharma.animesh@gmail.com>
2012-08-24 19:49:01 +0530

    check with even minutes

--- Ticker/src/Clock/Reminder/Check.java
+++ Ticker/src/Clock/Reminder/Check.java
@@ -1,24 +1,31 @@
 package Clock.Reminder;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
-import android.widget.AnalogClock;
-import android.widget.DigitalClock;
+import android.os.Vibrator;
+import java.util.Calendar;
 
-public class Check extends Activity
-{
-    /** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
- 
-		AnalogClock ac = (AnalogClock) findViewById(R.id.analogClock1);
-		//what can i do with AnalogClock?
- 
-		DigitalClock dc = (DigitalClock) findViewById(R.id.digitalClock1);
-		//what can i do with DigitalClock also? for display only
- 
-	}
- 
+public class Check extends Activity {
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //DigitalClock dc = (DigitalClock) findViewById(R.id.digitalClock1);
+        //if(dc.playSoundEffect(MODE_PRIVATE))
+        //what can i do with DigitalClock also? for display only
+        Calendar c = Calendar.getInstance();
+        int minutes = c.get(Calendar.MINUTE);
+        if (minutes%2 == 0) {
+            Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            v.vibrate(300);
+            setContentView(R.layout.main);
+        }
+    }
 }
+// http://stackoverflow.com/questions/5775973/how-can-give-vibrate-permission
+// http://stackoverflow.com/questions/5369682/android-get-current-time-and-date
