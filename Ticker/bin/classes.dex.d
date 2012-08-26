439e94d58951966d3047186a5aceab0728c17195
Ani <sharma.animesh@gmail.com>
2012-08-26 22:12:41 +0530

    HourlyReminder 1.0

--- Ticker/bin/classes.dex.d
+++ Ticker/bin/classes.dex.d
@@ -1,9 +1,9 @@
 C:\Users\Amithabh\Desktop\ani\nb\clock-ticker\Ticker\bin\classes.dex : \
 C:\Users\Amithabh\Desktop\ani\nb\clock-ticker\Ticker\bin\classes\Clock\Reminder\BuildConfig.class \
-C:\Users\Amithabh\Desktop\ani\nb\clock-ticker\Ticker\bin\classes\Clock\Reminder\Check$1.class \
 C:\Users\Amithabh\Desktop\ani\nb\clock-ticker\Ticker\bin\classes\Clock\Reminder\Check.class \
 C:\Users\Amithabh\Desktop\ani\nb\clock-ticker\Ticker\bin\classes\Clock\Reminder\HourlyReminder.class \
 C:\Users\Amithabh\Desktop\ani\nb\clock-ticker\Ticker\bin\classes\Clock\Reminder\R$attr.class \
+C:\Users\Amithabh\Desktop\ani\nb\clock-ticker\Ticker\bin\classes\Clock\Reminder\R$drawable.class \
 C:\Users\Amithabh\Desktop\ani\nb\clock-ticker\Ticker\bin\classes\Clock\Reminder\R$id.class \
 C:\Users\Amithabh\Desktop\ani\nb\clock-ticker\Ticker\bin\classes\Clock\Reminder\R$layout.class \
 C:\Users\Amithabh\Desktop\ani\nb\clock-ticker\Ticker\bin\classes\Clock\Reminder\R$string.class \
