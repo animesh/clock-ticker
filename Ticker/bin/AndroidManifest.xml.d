781b1cc1935b7884186a0ef24b49d3b648bd342b
Animesh Sharma <animeshs@animeshs-Latitude-E6230.NTNU>
2019-02-16 23:31:39 +0100

    updating to oreo

--- Ticker/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Amithabh\Desktop\ani\nb\clock-ticker\Ticker\bin\AndroidManifest.xml : \
-C:\Users\Amithabh\Desktop\ani\nb\clock-ticker\Ticker\AndroidManifest.xml \
--- Ticker/bin/Ticker.ap_.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Amithabh\Desktop\ani\nb\clock-ticker\Ticker\bin\Ticker.ap_ \
- : C:\Users\Amithabh\Desktop\ani\nb\clock-ticker\Ticker\res\drawable\fuzzylife.png \
-C:\Users\Amithabh\Desktop\ani\nb\clock-ticker\Ticker\res\values\strings.xml \
-C:\Users\Amithabh\Desktop\ani\nb\clock-ticker\Ticker\bin\res\drawable\fuzzylife.png \
-C:\Users\Amithabh\Desktop\ani\nb\clock-ticker\Ticker\bin\AndroidManifest.xml \
